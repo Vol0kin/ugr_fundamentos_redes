4690ed0d2e78d68c0c235058007ba39348e182ff
Jose Maria <jsg26398@gmail.com>
2018-10-30 17:51:28 +0100

    Ejercicio4 v1

---
+++ Ejercicio4/YodafyClienteUCP.java
@@ -0,0 +1,75 @@
+//
+// YodafyServidorIterativo
+// (CC) jjramos, 2012
+//
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress; 
+
+public class YodafyClienteUCP {
+
+	public static void main(String[] args) {
+		
+		int puerto=8989;
+		InetAddress direccion;
+		DatagramPacket paquete;
+		byte[] bufer = new byte[256];
+		DatagramSocket socket;
+		
+		try {
+			// Creamos un socket que se conecte a "host" y "port":
+			//////////////////////////////////////////////////////
+			socket = new DatagramSocket();
+			direccion = InetAddress.getByName("localhost");
+			//////////////////////////////////////////////////////			
+						
+			// Si queremos enviar una cadena de caracteres por un OutputStream, hay que pasarla primero
+			// a un array de bytes:
+			bufer = "Al monte del volcán debes ir sin demora".getBytes();
+			
+			// Enviamos el array por el outputStream;
+			//////////////////////////////////////////////////////
+			paquete = new DatagramPacket(bufer, bufer.length, direccion,puerto);
+			socket.send(paquete);
+			//////////////////////////////////////////////////////
+			
+			// Aunque le indiquemos a TCP que queremos enviar varios arrays de bytes, sólo
+			// los enviará efectivamente cuando considere que tiene suficientes datos que enviar...
+			// Podemos usar "flush()" para obligar a TCP a que no espere para hacer el envío:
+			//////////////////////////////////////////////////////
+			//outputStream.flush();
+			//////////////////////////////////////////////////////
+			
+			// Leemos la respuesta del servidor. Para ello le pasamos un array de bytes, que intentará
+			// rellenar. El método "read(...)" devolverá el número de bytes leídos.
+			//////////////////////////////////////////////////////
+			paquete = new DatagramPacket(bufer, bufer.length);
+			socket.receive(paquete);
+			paquete.getData();
+			paquete.getAddress();
+			paquete.getPort();
+			//////////////////////////////////////////////////////
+			
+			
+			// Una vez terminado el servicio, cerramos el socket (automáticamente se cierran
+			// el inpuStream  y el outputStream)
+			//////////////////////////////////////////////////////
+			socket.close();
+			//////////////////////////////////////////////////////
+			
+			// Excepciones:
+		} catch (UnknownHostException e) {
+			System.err.println("Error: Nombre de host no encontrado.");
+		} catch (IOException e) {
+			System.err.println("Error de entrada/salida al abrir el socket.");
+		}
+	}
+}
