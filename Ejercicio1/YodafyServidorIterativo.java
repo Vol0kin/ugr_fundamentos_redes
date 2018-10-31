c28fb1f4f597f03f29aba7acca64fa0791c779d4
Jose Maria <jsg26398@gmail.com>
2018-10-31 01:05:10 +0100

    Cambio servidor ejercicio 1

--- Ejercicio1/YodafyServidorIterativo.java
+++ Ejercicio1/YodafyServidorIterativo.java
@@ -20,12 +20,11 @@ public class YodafyServidorIterativo {
 		int bytesLeidos=0;
 
 		// Socket del servidor
-		ServerSocket serverSocket;
+		ServerSocket serverSocket = null;
 
 		try {
 			// Abrimos el socket en modo pasivo, escuchando el en puerto indicado por "port"
 			//////////////////////////////////////////////////
-			// ...serverSocket=... (completar)
 			System.out.println("Abriendo el socket del servidor en modo pasivo...");
 
 			serverSocket = new ServerSocket(port);
@@ -33,40 +32,37 @@ public class YodafyServidorIterativo {
 			System.out.println("Abierto socket en modo pasivo con exito");
 			//////////////////////////////////////////////////
 
-			// Mientras ... siempre!
-			do {
-				Socket socketServicio = null;
-
-				System.out.println("Esperando sockets...");
-				// Aceptamos una nueva conexión con accept()
-				/////////////////////////////////////////////////
-				// socketServicio=... (completar)
-				//////////////////////////////////////////////////
-
-				try {
-					socketServicio = serverSocket.accept();
-
-					System.out.println("Recibido socket cliente");
-					System.out.println("Yoda se prepara para procesar la peticion...");
-					// Creamos un objeto de la clase ProcesadorYodafy, pasándole como
-					// argumento el nuevo socket, para que realice el procesamiento
-					// Este esquema permite que se puedan usar hebras más fácilmente.
-					ProcesadorYodafy procesador=new ProcesadorYodafy(socketServicio);
-					procesador.procesa();
-
-					System.out.println("Yoda ha procesado la peticion. Respuesta enviada");
-
-					socketServicio.close();
-				} catch (IOException e) {
-					System.err.println("Error: no se pudo aceptar la conexion solicitada");
-				}
-
-
-			} while (true);
-
 		} catch (IOException e) {
 			System.err.println("Error al escuchar en el puerto "+port);
 		}
+
+		// Mientras ... siempre!
+		do {
+			Socket socketServicio = null;
+
+			System.out.println("Esperando sockets...");
+			// Aceptamos una nueva conexión con accept()
+			/////////////////////////////////////////////////
+
+			try {
+				socketServicio = serverSocket.accept();
+
+				System.out.println("Recibido socket cliente");
+				System.out.println("Yoda se prepara para procesar la peticion...");
+				// Creamos un objeto de la clase ProcesadorYodafy, pasándole como
+				// argumento el nuevo socket, para que realice el procesamiento
+				// Este esquema permite que se puedan usar hebras más fácilmente.
+
+			} catch (IOException e) {
+				System.err.println("Error: no se pudo aceptar la conexion solicitada");
+			}
+
+			ProcesadorYodafy procesador=new ProcesadorYodafy(socketServicio);
+			procesador.procesa();
+
+			System.out.println("Yoda ha procesado la peticion. Respuesta enviada");
+
+		} while (true);
 
 	}
 
