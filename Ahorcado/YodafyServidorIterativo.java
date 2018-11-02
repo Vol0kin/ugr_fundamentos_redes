da2c5faf07c3f0e071dac596df087e54e071d605
Jose Maria <jsg26398@gmail.com>
2018-11-02 23:42:57 +0100

    Ahorcado v2

---
+++ Ahorcado/YodafyServidorIterativo.java
@@ -0,0 +1,71 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.ServerSocket;
+
+
+//
+// YodafyServidorIterativo
+// (CC) jjramos, 2012
+//
+public class YodafyServidorIterativo {
+
+	public static void main(String[] args) {
+
+		// Puerto de escucha
+		int port=8989;
+		// array de bytes auxiliar para recibir o enviar datos.
+		byte []buffer=new byte[256];
+		// Número de bytes leídos
+		int bytesLeidos=0;
+
+		// Socket del servidor
+		ServerSocket serverSocket = null;
+
+		try {
+			// Abrimos el socket en modo pasivo, escuchando el en puerto indicado por "port"
+			//////////////////////////////////////////////////
+			System.out.println("Abriendo el socket del servidor en modo pasivo...");
+
+			serverSocket = new ServerSocket(port);
+
+			System.out.println("Abierto socket en modo pasivo con exito");
+			//////////////////////////////////////////////////
+
+		} catch (IOException e) {
+			System.err.println("Error al escuchar en el puerto "+port);
+		}
+
+		// Mientras ... siempre!
+		do {
+			Socket socketServicio = null;
+
+			System.out.println("Esperando sockets...");
+			// Aceptamos una nueva conexión con accept()
+			/////////////////////////////////////////////////
+
+			try {
+				socketServicio = serverSocket.accept();
+
+				System.out.println("Recibido socket cliente");
+				System.out.println("Ahorcado se prepara para procesar la peticion...");
+
+				// Creamos un objeto de la clase ProcesadorYodafy, pasándole como
+				// argumento el nuevo socket, para que realice el procesamiento
+				// Este esquema permite que se puedan usar hebras más fácilmente.
+
+				Ahorcado procesador=new Ahorcado(socketServicio);
+				procesador.ahorcame();
+
+				System.out.println("Ahorcado ha procesado la peticion. Respuesta enviada");
+			} catch (IOException e) {
+				System.err.println("Error: no se pudo aceptar la conexion solicitada");
+			}
+
+		} while (true);
+
+	}
+
+}
